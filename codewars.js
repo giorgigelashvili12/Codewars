549feeed6a34e2e2b1e7a1591c13968afbc97b0d
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-09-28 13:51:27 +0400

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -1,19 +1,92 @@
-// 1
-function getRealFloor(n) {
-    if(n <= 0) {
-        return n;
-    } else if (n >= 13) {
-        return n - 2;
+// 8kyu Student's Final Grade
+// status: passed
+function finalGrade (exam, projects) {
+    if(exam > 90 || projects > 10) {
+      return 100;
+    } else if (exam > 75 && projects >= 5) {
+      return 90;
+    } else if(exam > 50 && projects >= 2) {
+      return 75;
     } else {
-        return n -1;
+      return 0;
     }
 }
 
-// 2
-function dutyFree(normPrice, discount, hol){
-    let discounted = normPrice * (1 -discount / 100);
-  // 1 = saved money
-    let savedMoney = normPrice - discounted;
+// 8kyu Fake Binary
+// status: passed
+function fakeBin(x){
+    let result = "";
+    for(let i = 0; i < x.length; i++) {
+      if(x[i] < "5") {
+        result = result + "0";
+      } else {
+        result = result + "1"
+      }
+    }
+    
+    return result;
+}
+
+// 8kyu
+// status: failed (cant read properties of null - .lendgth)
+function countPositivesSumNegatives(input) {
+    let positives =0;
+    let nSum = 0;
+    
+    if(input.length === 0 || input == null) {
+      return [];
+    } 
   
-    return Math.floor(hol / savedMoney)
+    for(let i = 0; i < input.length; i++) {
+      if(input[i] > 0) {
+        positives = positives + 1;
+      } else if(input[i] < 0) {
+        nSum = (nSum + input[i]);
+      }
+    }
+    
+    return [positives, nSum];
+}
+
+// 7kyu Sum of two lowest positive integers
+// status: passed
+function sumTwoSmallestNumbers(numbers) {  
+    const lowestNum = Math.min(...numbers); // spread
+    const numIndex = numbers.indexOf(lowestNum);
+    numbers.splice(numIndex, 1)
+    
+    let secondLowest = Math.min(...numbers);
+    
+    return lowestNum + secondLowest;
+}
+
+// 7kyu Jaden Casing Strings
+// status: passed
+String.prototype.toJadenCase = function () {
+    let result = []
+    let texts = this.split(' ');
+    
+    for(let i = 0; i < texts.length; i++) {
+      let text = texts[i];
+      
+      result.push(text[0].toUpperCase() + text.slice(1));
+    }
+    
+    result = result.join(' ');
+    return result;
+};
+
+// 6kyu Stop gninnipS My sdroW!
+// status: passed
+function spinWords(string){
+    let text = string.split(' ');
+    for(let i = 0; i < text.length; i++) {
+      if(text[i].length >= 5) {
+        text[i] = text[i].split('')
+        text[i] = text[i].reverse()
+        text[i] = text[i].join('')
+      }
+    }
+    
+    return text.join(' ')
 }
